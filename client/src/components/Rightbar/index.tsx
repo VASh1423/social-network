f6cb43f84ca4c1e517fa605f5ff2d16c4b56c29e
VASh <shokarev.vl@gmail.com>
2022-01-29 12:18:34 +0300

    added rightbar

--- client/src/components/Rightbar/index.tsx
+++ client/src/components/Rightbar/index.tsx
@@ -5,7 +5,14 @@ export const Rightbar: React.FC = () => {
   return (
     <div className='rightbar'>
       <div className="rightbarContainer">
-        Leftbar
+        <h2 className='rightbarTitle'>Friends</h2>
+        <div className='rightbarList'>
+          <div className='rightbarList friend'>Friend Name 1</div>
+          <div className='rightbarList friend'>Friend Name 2</div>
+          <div className='rightbarList friend'>Friend Name 3</div>
+          <div className='rightbarList friend'>Friend Name 4</div>
+          <div className='rightbarList friend'>Friend Name 5</div>
+        </div>
       </div>
     </div>
   )
