b73a4a392891981719bddba6d251b1b2da9ef03b
VASh <shokarev.vl@gmail.com>
2022-02-23 12:52:52 +0300

    added register & login pages

--- client/src/Pages/Login/index.tsx
+++ client/src/Pages/Login/index.tsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './style.scss'
 
 export const Login: React.FC = () => {
   return (
-    <div>
-      Login
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginCenter">
+          <form className="loginBox">
+            <input 
+              placeholder="Email"
+              className="loginInput" 
+              type='email' 
+              required
+            />
+            <input 
+              placeholder="Password" 
+              type='password' 
+              className="loginInput" 
+              required
+              minLength={6}
+            />
+            <button className="loginButton" type="submit">
+              Войти
+            </button>
+            <span className="loginForgot">Забыли пароль?</span>
+            <Link to='/register'>
+              <button className="loginRegisterButton">
+                Создать новый аккаунт
+              </button>
+            </Link>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- client/src/Pages/Register/index.tsx
+++ client/src/Pages/Register/index.tsx
@@ -1,10 +1,46 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './style.scss'
 
 export const Register: React.FC = () => {
   return (
-    <div>
-      Register
+    <div className="register">
+      <div className="registerWrapper">
+        <div className="registerCenter">
+          <form className="registerBox">
+            <input 
+              placeholder="Username" 
+              className="registerInput" 
+            />
+            <input 
+              placeholder="Email" 
+              type='email' 
+              required 
+              className="registerInput" 
+            />
+            <input 
+              placeholder="Password" 
+              type='password' 
+              required 
+              className="registerInput" 
+              minLength={6}
+            />
+            <input 
+              placeholder="Password Again" 
+              type='password' 
+              required 
+              className="registerInput"
+              minLength={6}
+            />
+            <button className="registerButton" type='submit'>Зарегистрироваться</button>
+            <Link to='/login'>
+              <button className="registerLoginButton">
+                Войти в аккаунт
+              </button>
+            </Link>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
