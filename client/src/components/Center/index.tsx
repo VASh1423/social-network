0eda39ad108554c722397f1673689f97b0f1a7c3
VASh <shokarev.vl@gmail.com>
2022-03-06 21:58:02 +0300

    created post creator

--- client/src/components/Center/index.tsx
+++ client/src/components/Center/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { PostCreator } from '../PostCreator';
 import './style.scss'
 
 export const Center: React.FC = () => {
   return (
-    <div className='center'>
-      <div className="centerContainer">
-        Main page
+    <>
+      <PostCreator/>
+      <div className='center'>
+        <div className="centerContainer">
+          Main page
+        </div>
       </div>
-    </div>
+    </>
   )
 }
---
+++ client/src/components/PostCreator/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button } from '../../Templates/Button';
+import { Input } from '../../Templates/Input';
+import { AddAPhoto, MusicNote } from '@material-ui/icons';
+import './style.scss'
+
+export const PostCreator = () => {
+  return (
+    <div className='postCreator'>
+      <Input type='text' placeholder="What's new?"/>
+      <AddAPhoto style={{color: 'gray', marginLeft: '10px'}}/>
+      <MusicNote style={{color: 'gray', marginLeft: '10px'}}/>
+      <Button color='rgb(23, 117, 238)'>Send</Button>
+    </div>
+  )
+}
