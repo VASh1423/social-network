1dcad25caaa42c35d8891802f1860d8f89aeb745
VASh <shokarev.vl@gmail.com>
2022-03-11 21:41:28 +0300

    finished profile page

--- client/src/components/Profile/index.tsx
+++ client/src/components/Profile/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from '../../Templates/Button'
 import { Post } from '../Post'
 import { PostCreator } from '../PostCreator'
 import './style.scss'
@@ -15,7 +16,19 @@ export const Profile: React.FC = () => {
         </div>
       </div>
       <div className="centerProfileInfo">
-        
+        <div className="centerProfileInfoBtns">
+          <Button color='rgb(23, 117, 238)' >+ Add</Button>
+          <Button color='rgb(23, 117, 238)' >Write a message</Button>
+        </div>
+        <div className="centerProfileInfoInfo">
+          <h3 className='centerProfileInfoInfo name'>Full Name</h3>
+          <div className='centerProfileInfoInfo info'>
+            <p>Country: <b>USA</b></p>
+            <p>City: <b>Chicago</b></p>
+            <p>Age: <b>25</b></p>
+            <p>Date of birth: <b>03.12.1995</b></p>
+          </div>
+        </div>
       </div>
       <PostCreator/>
       <Post/>
