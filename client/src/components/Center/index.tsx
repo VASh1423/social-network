98c8844c35c59d9c27cefdad8f70728ff051e64a
VASh <shokarev.vl@gmail.com>
2022-04-20 09:33:43 +0300

    front added scroll to top

--- client/src/components/Center/index.tsx
+++ client/src/components/Center/index.tsx
@@ -1,10 +1,19 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Post } from '../Post';
+import { ArrowUpward } from '@material-ui/icons';
 import './style.scss'
 
 export const Center: React.FC = () => {
   const [posts, setPosts] = useState([])
+  const [scroll, setScroll] = useState(0);
+  const onScroll = () => setScroll(Math.round(window.scrollY))
+
+  useEffect(() => {
+    onScroll();
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [onScroll])
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -15,8 +24,19 @@ export const Center: React.FC = () => {
     fetchPosts()
   }, [])
 
+  const handlerScroll = () => {
+    window.scrollTo({top: 0, behavior: 'smooth'})
+  }
+
   return (
     <>
+      {scroll !== 0 && 
+        <div className="centerTopContainer">
+          <div className='centerIconToUp' onClick={handlerScroll}>
+            <ArrowUpward style={{position: 'relative'}}/>
+          </div>
+        </div>
+      }
       {posts.map((post, id) => <Post post={post} key={id}/>)}
     </>
   )
